8893e7108bfc768007059580ac787e0769a8aa0d
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2025-02-17 19:40:10 -0500

    faet: ok

--- frontend/src/app/models/Run.ts
+++ frontend/src/app/models/Run.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const runSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   time: { type: Number, required: true },
   distance: { type: Number, required: true },
   date: { type: Date, default: Date.now },
-})
+});
 
 export interface RunDocument extends mongoose.Document {
-  userId: mongoose.Types.ObjectId
-  time: number
-  distance: number
-  date: Date
+  userId: mongoose.Types.ObjectId;
+  time: number;
+  distance: number;
+  date: Date;
 }
 
-export default mongoose.models.Run || mongoose.model<RunDocument>("Run", runSchema)
-
+export default mongoose.model<RunDocument>("Run", runSchema);
