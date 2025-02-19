8ad513260b787e35d9de301363764f9bb762cb5b
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2025-02-19 15:52:46 -0500

    feat: yeah

--- backend/src/main/java/runtracker/repository/RunRepository.java
+++
@@ -1,7 +0,0 @@
-package runtracker.repository;
-
-import runtracker.model.Run;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RunRepository extends JpaRepository<Run, Long> {
-}
