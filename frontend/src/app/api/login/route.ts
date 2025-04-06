3529eabada3b0d2e89bb3637452aad809124f8ed
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2025-04-06 19:55:05 -0400

    feat: ok

--- frontend/src/app/api/login/route.ts
+++ frontend/src/app/api/login/route.ts
@@ -81,6 +81,7 @@ export async function POST(request: Request) {
 
     return NextResponse.json({ token }, { status: 200 });
   } catch (error) {
+    console.error("Error during login:", error);
     return NextResponse.json(
       { message: "Internal server error" },
       { status: 500 },
